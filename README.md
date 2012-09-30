0e77b4634d01d6feb39ccd158c9cc5d86ea0e708
Yanis Wang <yanis.wang@gmail.com>
2012-09-30 09:13:07 +0800

    首次发布代码

--- README.md
+++ README.md
@@ -2,3 +2,43 @@ MarkdownReader
 ==============
 
 Markdown Reader is a extention for chrome, used for reading markdown file.
+
+Features:
+==============
+
+1. Reading markdown file
+2. Auto reload local file when file is changed
+3. Highlight the code area
+4. Show outline beside the content
+
+License
+================
+
+hostsPlus is released under the MIT license:
+
+> The MIT License
+> 
+> Copyright (c) 2012 Yanis Wang \<yanis.wang@gmail.com\>
+> 
+> Permission is hereby granted, free of charge, to any person obtaining a copy
+> of this software and associated documentation files (the "Software"), to deal
+> in the Software without restriction, including without limitation the rights
+> to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+> copies of the Software, and to permit persons to whom the Software is
+> furnished to do so, subject to the following conditions:
+> 
+> The above copyright notice and this permission notice shall be included in
+> all copies or substantial portions of the Software.
+> 
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+> IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+> FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+> AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+> LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+> OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+> THE SOFTWARE.
+
+Thanks
+================
+
+* GitHub: [https://github.com/](https://github.com/)
---
+++ markdownreader.js
@@ -0,0 +1,96 @@
+(function(document) {
+
+	//忽略HTML代码
+	if (document.doctype) return;
+
+	var link = document.createElement('link');
+	link.rel = 'stylesheet';
+	link.href = chrome.extension.getURL('markdownreader.css');
+	document.head.appendChild(link);
+
+	link = document.createElement('link');
+	link.rel = 'stylesheet';
+	link.href = chrome.extension.getURL('prettify.css');
+	document.head.appendChild(link);
+	document.body.innerHTML = '<div id="markdown-container"></div><div id="markdown-outline"></div><div id="markdown-backTop" onclick="window.scrollTo(0,0);"></div>';
+	window.onresize = showOutline;
+
+	var markdownConverter = new Showdown.converter();
+	var lastText = null;
+
+	function updateMarkdown(text) {
+		if (text !== lastText) {
+			lastText = text;
+			markdownConverter.makeHtml(lastText)
+			document.getElementById('markdown-container').innerHTML = markdownConverter.makeHtml(lastText);
+			prettyPrint();
+			updateOutline();
+		}
+	}
+
+	function updateOutline() {
+		var arrAllHeader = document.querySelectorAll("h1,h2,h3,h4,h6,h6");
+		var arrOutline = ['<ul>'];
+		var header, headerText;
+		var level = 0,
+			lastLevel = 1;
+		var levelCount = 0;
+		for (var i = 0, c = arrAllHeader.length; i < c; i++) {
+			header = arrAllHeader[i];
+			headerText = header.innerText;
+
+			header.setAttribute('id', headerText);
+
+			level = header.tagName.match(/^h(\d)$/i)[1];
+			levelCount = level - lastLevel;
+
+			if (levelCount > 0) {
+				for (var j = 0; j < levelCount; j++) {
+					arrOutline.push('<ul>');
+				}
+			} else if (levelCount < 0) {
+				levelCount *= -1;
+				for (var j = 0; j < levelCount; j++) {
+					arrOutline.push('</ul>');
+				}
+			};
+			arrOutline.push('<li>');
+			arrOutline.push('<a href="#' + headerText + '">' + headerText + '</a>');
+			arrOutline.push('</li>');
+			lastLevel = level;
+		}
+		arrOutline.push('</ul>')
+		var outline = document.getElementById('markdown-outline');
+		if(arrOutline.length > 2){
+			outline.innerHTML = arrOutline.join('');
+			showOutline();
+		}
+		else outline.style.display = 'none'; 
+	}
+
+	function showOutline() {
+		var outline = document.getElementById('markdown-outline');
+		var markdownContainer = document.getElementById('markdown-container');
+		outline.style.left = markdownContainer.offsetLeft + markdownContainer.offsetWidth + 10 + 'px';
+		outline.style.display = 'block';
+	}
+
+	var xmlhttp = new XMLHttpRequest();
+	var fileurl = location.href,
+		bLocalFile = /^file:\/\//i.test(fileurl);
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status != 404) {
+			updateMarkdown(xmlhttp.responseText);
+		}
+	};
+
+	function checkUpdate() {
+		xmlhttp.abort();
+		xmlhttp.open("GET", fileurl + '?rnd=' + new Date().getTime(), true);
+		xmlhttp.send(null);
+		if (bLocalFile) setTimeout(checkUpdate, 500);
+	}
+
+	checkUpdate();
+
+}(document));
